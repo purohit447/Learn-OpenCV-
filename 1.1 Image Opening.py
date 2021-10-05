44104675ab27d11d040daacd3343a817ef822059
Karan Purohit <76615863+purohit447@users.noreply.github.com>
2021-10-05 14:09:55 +0530

    Update 1.1 Image Opening.py

--- 1.1 Image Opening.py
+++ 1.1 Image Opening.py
@@ -5,10 +5,10 @@ import numpy as np
 
 image=cv2.imread('#image_name_with_extension') # Reading Image
 
-win_name='my application' # Kernal_Name
+win_name='my application' # Kernel_Name
 
 cv2.imshow(win_name,image) # Show_Image 
 
-cv2.waitKey(0)  # For Terminating the Kernal
+cv2.waitKey(0)  # For Terminating the Kernel
 
 cv2.destroyAllWindows() # Destroy all windows
