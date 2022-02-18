4a79fd2760bf163c78574a812b1b28f85ff5e29b
Karan Purohit <76615863+purohit447@users.noreply.github.com>
2022-02-18 17:19:39 +0530

    Update 2.2 Shape appearance.py

--- 2.2 Shape appearance.py
+++ 2.2 Shape appearance.py
@@ -11,7 +11,7 @@ cv2.line(canvas, (200,210),(450,450),(255,250,0),3, cv2.LINE_8)
 # type AA is the application of gausian filtering
 cv2.line(canvas, (100,140),(400,450),(255,250,0),3, cv2.LINE_AA)
 
-cv2.rectangle(canvas, (210,10),(370,150),(250,0,0),-1) # rectangle(image_name,((diagonal-1) x1,y1),((diagonal-2) x2,y2), (B,G,R), fill/thickness)
+cv2.rectangle(canvas, (210,10),(370,150),(250,0,0),-1) # rectangle(image_name,((diagonal:1) x1,y1),((diagonal:2) x2,y2), (B,G,R), fill/thickness)
 cv2.circle(canvas,(150,300),100,(0,255,0),-3) #circle (image_name, ((center)x,y), radius, (B,G,R), thickness/fill)
 cv2.arrowedLine(canvas,(400,450),(470,450),(255,255,0),tipLength=0.3) # arrowLine(image_name,(x1,y1),(x2,y2),(B,G,R),tipLenght)
 
